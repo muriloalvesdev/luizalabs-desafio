d734884af602905eaf4da7e2fc0c63e3ba7da2f6
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-03 23:07:01 -0300

    Exceptions

---
+++ src/main/java/br/com/schedule/exception/ScheduleNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.schedule.exception;
+
+public class ScheduleNotFoundException extends RuntimeException {
+
+  public ScheduleNotFoundException(String uuid) {
+    super("Schedule not found with uuid [ " + uuid + " ] informed ");
+  }
+
+}
---
+++ src/main/java/br/com/schedule/exception/TypeNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.schedule.exception;
+
+public class TypeNotFoundException extends RuntimeException {
+
+  public TypeNotFoundException(String message) {
+    super(message);
+  }
+}
