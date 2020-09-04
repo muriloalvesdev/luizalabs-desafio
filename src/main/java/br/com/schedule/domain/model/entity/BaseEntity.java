ebe5f5c67bbe9125d18a9cf364421e78493f0761
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-03 23:03:21 -0300

    Create Base Entities

---
+++ src/main/java/br/com/schedule/domain/model/entity/BaseEntity.java
@@ -0,0 +1,42 @@
+package br.com.schedule.domain.model.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+  private static final long serialVersionUID = 8034676095293542006L;
+
+  @Column(name = "created_at")
+  @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+  protected LocalDateTime createdAt;
+
+  @Column(name = "updated_at")
+  @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+  protected LocalDateTime updatedAt;
+
+  @PrePersist
+  protected void onCreate() {
+    this.createdAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    this.updatedAt = LocalDateTime.now();
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return this.updatedAt == null ? getCreatedAt() : this.updatedAt;
+  }
+
+}
