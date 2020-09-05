3ab1dd80b3b78dce195c904ee92d3903cb87100e
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-04 21:45:10 -0300

    add method findByStatus for find schedule by Status and add method findByUuidAndStatus for delete schedule with status pending

--- src/main/java/br/com/schedule/domain/repository/ScheduleRepository.java
+++ src/main/java/br/com/schedule/domain/repository/ScheduleRepository.java
@@ -2,12 +2,17 @@ package br.com.schedule.domain.repository;
 
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.schedule.domain.model.entity.Recipient;
 import br.com.schedule.domain.model.entity.Schedule;
+import br.com.schedule.domain.model.entity.Status;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, UUID> {
-
   Optional<Schedule> findByRecipient(Recipient find);
 
+  Optional<Schedule> findByUuidAndStatus(UUID uuid, Status status);
+
+  Page<Schedule> findByStatus(Status status, Pageable pageable);
 }
