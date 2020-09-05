02c58df1b42763e302b7f463ec9692681b908365
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-04 22:10:17 -0300

    test of ConvertRecipient

---
+++ src/test/java/br/com/schedule/convert/ConvertRecipientTest.java
@@ -0,0 +1,31 @@
+package br.com.schedule.convert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import br.com.schedule.domain.model.entity.Recipient;
+import br.com.schedule.dto.RecipientDataTransferObject;
+
+class ConvertRecipientTest {
+  private static final String RECIPIENT = "murilohenrique.ti@outlook.com.br";
+
+  @Test
+  void shouldConvertDataTransferObjectToEntity() {
+    RecipientDataTransferObject dto = createRecipientDataTransferObject();
+    Recipient recipient = ConvertRecipient.toEntity(dto);
+
+    assertEquals(dto.getRecipient(), recipient.getRecipient());
+  }
+
+  @Test
+  void shouldConvertEntityToDataTransferObject() {
+    Recipient recipient = Recipient.newBuilder().recipient(RECIPIENT).build();
+    RecipientDataTransferObject dto = ConvertRecipient.toDataTransferObject(recipient);
+
+    assertEquals(recipient.getRecipient(), dto.getRecipient());
+  }
+
+  private RecipientDataTransferObject createRecipientDataTransferObject() {
+    return RecipientDataTransferObject.newBuilder().recipient(RECIPIENT).build();
+  }
+
+}
