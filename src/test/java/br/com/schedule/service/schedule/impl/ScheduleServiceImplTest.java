b5afb392df9e425b77c03adb2993ee917ecedd9a
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-05 00:39:18 -0300

    correction primary test created and create new test for seconde case 'shouldDeleteLogicallySchedule'

--- src/test/java/br/com/schedule/service/schedule/impl/ScheduleServiceImplTest.java
+++ src/test/java/br/com/schedule/service/schedule/impl/ScheduleServiceImplTest.java
@@ -3,10 +3,14 @@ package br.com.schedule.service.schedule.impl;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import br.com.schedule.ConstantsTests;
 import br.com.schedule.domain.model.entity.Recipient;
 import br.com.schedule.domain.model.entity.Schedule;
 import br.com.schedule.domain.repository.ScheduleRepository;
@@ -14,17 +18,28 @@ import br.com.schedule.dto.RecipientDataTransferObject;
 import br.com.schedule.dto.ScheduleDataTransferObject;
 import br.com.schedule.service.recipient.RecipientService;
 
-class ScheduleServiceImplTest {
+class ScheduleServiceImplTest implements ConstantsTests {
 
   private ScheduleRepository repository;
+
   private ScheduleServiceImpl service;
   private RecipientService recipientService;
 
-  private ScheduleDataTransferObject dto;
+  private ScheduleDataTransferObject scheduleDataTransferObject;
+  private RecipientDataTransferObject recipientDataTransferObject;
+
+  Schedule schedule;
 
   @BeforeEach
   void setUp() {
-    this.dto = ScheduleDataTransferObject.newBuilder().build();
+    schedule = Schedule.newBuilder().build();
+    recipientDataTransferObject =
+        RecipientDataTransferObject.newBuilder().recipient(RECIPIENT).build();
+
+    this.scheduleDataTransferObject = ScheduleDataTransferObject.newBuilder().message(MESSAGE)
+        .sendDate(SEND_DATE).sendDate(LocalDateTime.now().plusDays(7L)).status(PENDING.name())
+        .type(EMAIL.name()).recipient(recipientDataTransferObject).build();
+
     this.repository = Mockito.spy(ScheduleRepository.class);
     this.recipientService = Mockito.spy(RecipientService.class);
 
@@ -33,18 +48,28 @@ class ScheduleServiceImplTest {
 
   @Test
   void shouldSaveSchedule() {
-    Schedule schedule = Schedule.newBuilder().build();
     Recipient recipient = Recipient.newBuilder().build();
-    RecipientDataTransferObject recipientDataTransferObject =
-        RecipientDataTransferObject.newBuilder().build();
 
-    BDDMockito.given(this.repository.saveAndFlush(schedule)).willReturn(schedule);
+    BDDMockito.given(this.repository.saveAndFlush(any(Schedule.class))).willReturn(schedule);
     BDDMockito.given(this.recipientService.save(recipientDataTransferObject)).willReturn(recipient);
 
-    service.save(dto);
+    service.save(scheduleDataTransferObject);
 
     verify(this.recipientService, times(1)).save(any(RecipientDataTransferObject.class));
-    verify(this.repository, times(1)).save(any(Schedule.class));
+    verify(this.repository, times(1)).saveAndFlush(any(Schedule.class));
+  }
+
+  @Test
+  void shouldDeleteLogicallySchedule() {
+    UUID uuid = UUID.randomUUID();
+    BDDMockito.given(this.repository.findByUuidAndStatus(uuid, PENDING))
+        .willReturn(Optional.of(this.schedule));
+    BDDMockito.given(this.repository.save(any(Schedule.class))).willReturn(this.schedule);
+
+    this.service.delete(uuid.toString());
+
+    verify(repository, times(1)).findByUuidAndStatus(uuid, PENDING);
+    verify(repository, times(1)).save(any(Schedule.class));
   }
 
 }
