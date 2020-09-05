dffb98c405f95a890469545976582dc6bbb20c0a
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-04 21:41:16 -0300

    add search StatusName enum

--- src/main/java/br/com/schedule/domain/model/entity/Status.java
+++ src/main/java/br/com/schedule/domain/model/entity/Status.java
@@ -1,6 +1,25 @@
 package br.com.schedule.domain.model.entity;
 
+import java.util.Arrays;
+import br.com.schedule.exception.StatusNotFoundException;
+
 public enum Status {
-  PENDING, SENT, CANCELLED;
+  PENDING("PENDING"), SENT("SENT"), DELETED("DELETED");
+
+  private String status;
+
+  private Status(String status) {
+    this.status = status;
+  }
+
+  private String getStatus() {
+    return status;
+  }
+
+  public static Status find(String status) {
+    return Arrays.asList(Status.values()).stream()
+        .filter(s -> s.getStatus().equals(status.toUpperCase())).findFirst()
+        .orElseThrow(() -> new StatusNotFoundException(status));
+  }
 
 }
