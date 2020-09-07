d6aee284ecb4336085f457ecdc231741b91375ad
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-07 11:59:18 -0300

    remove method fixed not used

--- src/test/java/br/com/schedule/service/recipient/impl/RecipientServiceImplTest.java
+++ src/test/java/br/com/schedule/service/recipient/impl/RecipientServiceImplTest.java
@@ -3,13 +3,9 @@ package br.com.schedule.service.recipient.impl;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
@@ -42,16 +38,5 @@ class RecipientServiceImplTest implements ConstantsTests {
     this.service.save(this.dto);
 
     verify(this.repository, times(1)).save(any(Recipient.class));
-  }
-
-
-  static class RecipientProviderForTests implements ArgumentsProvider {
-
-    @Override
-    public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
-      return Stream
-          .of(RecipientDataTransferObject.newBuilder().recipient(ConstantsTests.RECIPIENT).build())
-          .map(Arguments::of);
-    }
   }
 }
