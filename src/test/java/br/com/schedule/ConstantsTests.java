72056a7fcc934e69460d62a20e05fbcc5bf9e935
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-07 15:54:03 -0300

    add to constantsTests attribute PHONE for Recipients

--- src/test/java/br/com/schedule/ConstantsTests.java
+++ src/test/java/br/com/schedule/ConstantsTests.java
@@ -5,8 +5,9 @@ import br.com.schedule.domain.model.entity.Status;
 import br.com.schedule.domain.model.entity.Type;
 
 public interface ConstantsTests {
-  static final String RECIPIENT = "murilohenrique.ti@outlook.com.br";
-  
+  static final String RECIPIENT_EMAIL = "murilohenrique.ti@outlook.com.br";
+  static final String RECIPIENT_PHONE = "55-16-994496217";
+
   static final LocalDateTime SEND_DATE = LocalDateTime.now().plusDays(7L);
   static final String MESSAGE = "VOCÊ PASSOU EM NOSSO PROCESSO SELETIVO, BEM VINDO!";
   static final Status PENDING = Status.PENDING;
