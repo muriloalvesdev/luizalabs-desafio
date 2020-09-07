ab112b1dbbf82469fc7cb5058f062d71ce2db425
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-07 12:08:43 -0300

    name the tests

--- src/test/java/br/com/schedule/convert/recipient/validation/ValidationRecipientTest.java
+++ src/test/java/br/com/schedule/convert/recipient/validation/ValidationRecipientTest.java
@@ -1,5 +1,6 @@
 package br.com.schedule.convert.recipient.validation;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,17 +8,31 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Classe responsável por testar a clase de Validação do Recipient")
 class ValidationRecipientTest {
 
-  private static final String EMAIL_VALUE = "murilohenrique.ti@outlook.com.br";
-  private static final String PHONE_VALUE = "55-16-994496217";
+  private static final String EMAIL_VALID = "murilohenrique.ti@outlook.com.br";
+  private static final String PHONE_VALID = "55-16-994496217";
+
+  private static final String EMAIL_NOT_VALID = "murilohenrique.tioutlook.com.br";
+  private static final String PHONE_NOT_VALID = "55 16 99449 6217";
 
 
+  @DisplayName("Deve enviar o email e o telefone para validação e retornar 'true', pois ambos são válidos")
   @Test
   void shouldValidate() {
-    boolean emailValidated = ValidationRecipient.isValid(EMAIL_VALUE);
-    boolean phoneValidated = ValidationRecipient.isValid(PHONE_VALUE);
-    
+    boolean emailValidated = ValidationRecipient.isValid(EMAIL_VALID);
+    boolean phoneValidated = ValidationRecipient.isValid(PHONE_VALID);
+
     assertTrue(emailValidated);
     assertTrue(phoneValidated);
+  }
+
+  @DisplayName("Deve enviar o email e o telefone para validação e retornar 'false', pois ambos estão inválidos")
+  @Test
+  void shouldReturnInvalid() {
+    boolean emailValidated = ValidationRecipient.isValid(EMAIL_NOT_VALID);
+    boolean phoneValidated = ValidationRecipient.isValid(PHONE_NOT_VALID);
+
+    assertFalse(emailValidated);
+    assertFalse(phoneValidated);
   }
 
 }
