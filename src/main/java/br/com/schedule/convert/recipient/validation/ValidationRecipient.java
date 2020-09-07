b6ca3508b49f139300ba8819b24c5baf1a11bf8d
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-07 10:16:31 -0300

    improving way to return true in the validation

--- src/main/java/br/com/schedule/convert/recipient/validation/ValidationRecipient.java
+++ src/main/java/br/com/schedule/convert/recipient/validation/ValidationRecipient.java
@@ -12,12 +12,9 @@ public final class ValidationRecipient {
   private static final String REGEX_MATCHER_THIRTEEN_DIGITS_NUMBER = "\\d{2}-\\d{2}-\\d{9}";
 
   public static boolean isValid(String recipient) {
-    if (EmailValidator.getInstance().isValid(recipient)) {
-      return true;
-    }
     Pattern pattern = Pattern.compile(REGEX_MATCHER_THIRTEEN_DIGITS_NUMBER);
     Matcher matcher = pattern.matcher(recipient);
-    return matcher.matches();
+    return matcher.matches() || (EmailValidator.getInstance().isValid(recipient));
   }
 
 }
