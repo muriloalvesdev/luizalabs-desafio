ebbf3a1db6875b0b5de4940af127815ce95e1594
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-10-06 20:20:50 -0300

    importação não utilizada

--- src/main/java/br/com/schedule/service/recipient/impl/RecipientServiceImpl.java
+++ src/main/java/br/com/schedule/service/recipient/impl/RecipientServiceImpl.java
@@ -3,7 +3,6 @@ package br.com.schedule.service.recipient.impl;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
 import br.com.schedule.convert.recipient.ConvertRecipient;
-import br.com.schedule.domain.model.entity.Recipient;
 import br.com.schedule.domain.repository.RecipientRepository;
 import br.com.schedule.dto.RecipientDataTransferObject;
 import br.com.schedule.service.recipient.RecipientService;
